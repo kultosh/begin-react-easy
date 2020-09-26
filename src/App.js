2ca0bd81902db9fe7789cd48489ff44be9451d19
kultosh <kulgole90@gmail.com>
2020-09-26 22:07:40 +0545

    Setup React with TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <h2>React Begining</h2>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <footer className="bg-gray-200 text-center p-3 text-xs absolute bottom-0 w-full">
+                &copy; Copyright 2020
+        </footer>
+    )
+}
+
+export default Footer
