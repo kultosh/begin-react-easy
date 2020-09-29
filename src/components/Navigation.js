4828ba9c853fccc1bb881ab43c33f36fd2d28fd0
kultosh <kulgole90@gmail.com>
2020-09-29 07:25:22 +0545

    Animated the navigation by using react spring package

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,57 @@
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {useTransition, animated} from 'react-spring';
 
 function Navigation() {
     const[showMenu, setShowMenu] = useState(false);
-    let menu, menuMask;
-    
-    if(showMenu)
-    {
-        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">Hello</div>
 
-        menuMask = <div className="fixed bg-black-t-50 top-0 left-0 w-full h-full z-50" onClick = {() => setShowMenu(!showMenu)}></div>
-    }
+    const maskTransitions = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        });
+
+    const menuTransitions = useTransition(showMenu, null, {
+        from: { opacity: 0, transform: "translateX(-100%)" },
+        enter: { opacity: 1, transform: "translateX(0%)" },
+        leave: { opacity: 0, transform: "translateX(-100%)" },
+        });
+    
     return(
         <nav>
             <span className="text-xl">
             <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
             </span>
-            {menuMask}
-            {menu}
+
+            {
+                maskTransitions.map(({item,key,props}) =>
+                    item && < animated.div key = {
+                        key
+                    }
+                    style = {props}
+                    className = "fixed bg-black-t-50 top-0 left-0 w-full h-full z-50"
+                    onClick = {
+                        () => setShowMenu(false)
+                    } ></animated.div>
+                )
+            }
+
+        {
+            menuTransitions.map(({item,key,props}) =>
+                item && < animated.div key = {
+                    key
+                }
+                style = {props}
+                className = "fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow" > 
+                <span className="font-bold">The Menu</span>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+                </animated.div>
+            )
+        }
         </nav>
     )
 }
