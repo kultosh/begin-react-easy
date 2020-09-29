45ff23153b063da6d51b5f6c951fd3cc551e1de0
kultosh <kulgole90@gmail.com>
2020-09-29 06:46:34 +0545

    Created Navigation Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 function Header() {
     return(
-        <header className="border-b font-bold p-3">
-                This is Title
+        <header className="border-b p-3 flex justify-between items-center">
+                <span className="font-bold">Title</span>
+                <Navigation />
         </header>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'black-t-50': 'rgba(0,0,0,0.5)',
+      }
+    },
   },
   variants: {},
   plugins: [],
