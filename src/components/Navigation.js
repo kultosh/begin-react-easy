0c636db75302b43da6c4d95e65b22dfff18bebc0
kultosh <kulgole90@gmail.com>
2020-09-30 10:30:44 +0545

    Revert "No changes"
    This reverts commit 7fa5bc0c4f9a012c4e4f1f7d200956f0a22b82fb.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import {useTransition, animated} from 'react-spring';
+import {Link} from 'react-router-dom';
 import NavigationMenu from './NavigationMenu';
 
 function Navigation() {
