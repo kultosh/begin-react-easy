863328fcc981a5968f0a26af3d7dd3026ba92c97
kultosh <kulgole90@gmail.com>
2020-10-02 06:01:56 +0545

    Created loader at the time of fetching data

--- src/views/Product.js
+++ src/views/Product.js
@@ -5,29 +5,44 @@ import {useParams} from 'react-router-dom';
 function Product() {
     const {id} = useParams();
     const url = `https://5f751ebf1cf3c900161cdccc.mockapi.io/products/${id}`;
-    const [product, setProduct] = useState(null);
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    });
     let content = null;
     
     useEffect(() => {
+        setProduct(({
+            loading: true,
+            data: null
+        }))
         Axios.get(url)
         .then(response => {
-            setProduct(response.data);
+            setProduct({
+                loading: false,
+                data: response.data
+            });
         })
     }, [url]);
 
-    if(product)
+    if(product.loading)
+    {
+       content = <span>..loading</span>
+    }
+
+    if(product.data)
     {
         content =
         <div>
-            <h1 className="font-bold text-2xl mb-3">{product.name}</h1>
+            <h1 className="font-bold text-2xl mb-3">{product.data.name}</h1>
             <div>
-                <img src={product.image[0].image_url} alt={product.name} />
+                <img src={product.data.image[0].image_url} alt={product.data.name} />
             </div>
             <div className="font-bold text-xl mb-3">
-                $ {product.price}
+                $ {product.data.price}
             </div>
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
