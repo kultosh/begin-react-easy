39029e441da69fddcbb012e74efda60caf2dacf2
kultosh <kulgole90@gmail.com>
2020-10-02 06:40:43 +0545

    Created Axios catch error

--- src/views/Product.js
+++ src/views/Product.js
@@ -8,23 +8,38 @@ function Product() {
     const url = `https://5f751ebf1cf3c900161cdccc.mockapi.io/products/${id}`;
     const [product, setProduct] = useState({
         loading: false,
-        data: null
+        data: null,
+        error: null
     });
     let content = null;
     
     useEffect(() => {
         setProduct(({
             loading: true,
-            data: null
+            data: null,
+            error: null
         }))
         Axios.get(url)
         .then(response => {
             setProduct({
                 loading: false,
-                data: response.data
-            });
+                data: response.data,
+                error: null
+            })
+        })
+        .catch(() => {
+                setProduct({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
         })
     }, [url]);
+
+    if(product.error)
+    {
+        content = <p>No Data Found!</p>
+    }
 
     if(product.loading)
     {
